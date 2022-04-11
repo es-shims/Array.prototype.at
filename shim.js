7d9840f124331582a597ec2c57070e29ff739977
Jordan Harband <ljharb@gmail.com>
2022-04-11 13:24:43 -0700

    [New] `shim`/`auto`: add `at` to `Symbol.unscopables`

--- shim.js
+++ shim.js
@@ -1,14 +1,20 @@
 'use strict';
 
 var define = require('define-properties');
+var shimUnscopables = require('es-shim-unscopables');
+
 var getPolyfill = require('./polyfill');
 
 module.exports = function shimArrayPrototypeAt() {
 	var polyfill = getPolyfill();
+
 	define(
 		Array.prototype,
 		{ at: polyfill },
 		{ at: function () { return Array.prototype.at !== polyfill; } }
 	);
+
+	shimUnscopables('at');
+
 	return polyfill;
 };
