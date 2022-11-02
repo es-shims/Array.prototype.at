4ede643ccb6b87acdc91a054f9710361f14a93c5
Jordan Harband <ljharb@gmail.com>
2022-11-02 10:38:36 -0700

    [Deps] update `define-properties`, `es-abstract`

--- implementation.js
+++ implementation.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var Get = require('es-abstract/2021/Get');
-var LengthOfArrayLike = require('es-abstract/2021/LengthOfArrayLike');
-var ToIntegerOrInfinity = require('es-abstract/2021/ToIntegerOrInfinity');
-var ToObject = require('es-abstract/2021/ToObject');
-var ToString = require('es-abstract/2021/ToString');
+var Get = require('es-abstract/2022/Get');
+var LengthOfArrayLike = require('es-abstract/2022/LengthOfArrayLike');
+var ToIntegerOrInfinity = require('es-abstract/2022/ToIntegerOrInfinity');
+var ToObject = require('es-abstract/2022/ToObject');
+var ToString = require('es-abstract/2022/ToString');
 
 module.exports = function at(index) {
 	var O = ToObject(this);
